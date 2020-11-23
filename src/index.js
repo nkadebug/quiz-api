4f83d6db95e9be53d6ec44969de770d104fb892b
nkadebug <nka.debug@gmail.com>
2020-11-23 02:55:59 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,1 @@
+require("./spreadhseet.data").getIndexJson();
---
+++ src/spreadhseet.data.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const fs = require("fs");
+
+exports.getIndexJson = function () {
+  const index_url =
+    "https://script.google.com/macros/s/AKfycbw9XAGHXLNfPy-ubSa1KCC51TK78IWE1WdAXXSVuHMLcGF5Recu/exec";
+
+  axios
+    .get(index_url)
+    .then(function ({ data }) {
+      console.log(data);
+      fs.writeFile("docs/index.json", JSON.stringify(data, null, 2), function (
+        err
+      ) {
+        if (err) throw err;
+        console.log(err);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
